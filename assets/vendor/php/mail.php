96ede6ff1273072fe54c52bc0cad8d47466393ef
Gaston Rodriguez <garodriguez@arnaldocastro.com.uy>
2023-09-19 00:19:02 -0300

    trabajando en el envio de mails

--- assets/vendor/php/mail.php
+++ assets/vendor/php/mail.php
@@ -1,25 +1,25 @@
 <?php
-use assets\vendor\PHPMailer\PHPMailer;
-use assets\vendor\PHPMailer\Exception;
+use PHPMailer\PHPMailer;
+use PHPMailer\Exception;
 
-require 'assets\vendor\PHPMailer';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
+require './PHPMailer/Exception';
+require './PHPMailer/PHPMailer.php';
+require './PHPMailer/SMTP.php';
 
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //Server settings
     $mail->SMTPDebug = 0;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'http://mail.tecnoaluminio.com.uy';  // Specify main and backup SMTP servers
+    $mail->Host = 'mail.tecnoaluminio.com.uy';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = ' consultas@tecnoaluminio.com.uy';                 // SMTP username
     $mail->Password = 'T9(vV]pKgk0v';                           // SMTP password
-    $mail->SMTPSecure = 'STARTTLS';                           
-    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->SMTPSecure = 'ssl';                           
+    $mail->Port = 465;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('info@example.net');
+    $mail->setFrom('consultas@tecnoaluminio.com.uy', 'Consultas Tecnoaluminio');
     $mail->addAddress($_POST['mail']);     // Add a recipient
 
 
